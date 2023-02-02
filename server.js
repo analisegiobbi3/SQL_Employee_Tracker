7b1d221665591661f6ee415cb1ca7f2714fcbf84
Analise <analise.giobbi@gc.com>
2023-02-02 12:28:28 -0500

    jump start on homework. Adding inquirer parts

--- server.js
+++ server.js
@@ -1,1 +1,78 @@
 const inquirer = require('inquirer')
+
+const viewOptions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'tableOptions',
+            message: 'Query options: ',
+            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
+        },
+    ])
+};
+
+
+const addDepartment = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'Enter department name: ',
+        },
+    ])
+};
+
+const addRole = () =>{
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter role: ',
+        },
+
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter employee salary: ',
+        },
+
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Enter employee department: ',
+        },
+    ])
+};
+
+const addEmployee = () =>{
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Enter employee first name: ',
+        },
+
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Enter employee last name: ',
+        },
+
+        {
+            type: 'input',
+            name: 'role',
+            message: 'Enter employee role: ',
+        },
+
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'Enter employee manager: ',
+        },
+    ])
+    
+}
+
+const updateEmployee = () =>{
+    
+}
